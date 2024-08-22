8f320da400b1f47f38629851bc51f40ba5ed2937
Seooyooungg <151813830+Seooyooungg@users.noreply.github.com>
2024-08-22 22:45:26 +0900

    네비게이션 바 추가
    하단 네비게이션 바 생성

--- app/src/main/java/com/bestdriver/aaa_klaxon/MainActivity.kt
+++ app/src/main/java/com/bestdriver/aaa_klaxon/MainActivity.kt
@@ -7,16 +7,45 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bestdriver.aaa_klaxon.home.BottomBar
 import com.bestdriver.aaa_klaxon.home.ListCard
 import com.bestdriver.aaa_klaxon.home.MapCard
 import com.bestdriver.aaa_klaxon.home.TitleCard
 import com.bestdriver.aaa_klaxon.ui.theme.AAA_klaxonTheme
+
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val hasNews: Boolean
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,9 +53,65 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AAA_klaxonTheme {
+                val items = listOf(
+                    BottomNavigationItem(
+                        title = "커뮤니티",
+                        selectedIcon = Icons.Filled.Email,
+                        unselectedIcon = Icons.Outlined.Email,
+                        hasNews = false,
+                    ),
+                    BottomNavigationItem(
+                        title = "홈",
+                        selectedIcon = Icons.Filled.Home,
+                        unselectedIcon = Icons.Outlined.Home,
+                        hasNews = false,
+                    ),
+                    BottomNavigationItem(
+                        title = "마이페이지",
+                        selectedIcon = Icons.Filled.Person,
+                        unselectedIcon = Icons.Outlined.Person,
+                        hasNews = false,
+                    ),
+                )
+                var selectedItemIndex by rememberSaveable {
+                    mutableStateOf(0)
+                }
                 // Use Scaffold to include the BottomBar and manage layout
                 Scaffold(
-                    bottomBar = { BottomBar() }
+                    bottomBar = {
+                        NavigationBar {
+                            items.forEachIndexed { index, item ->
+                                NavigationBarItem(
+                                    selected = selectedItemIndex == index,
+                                    onClick = {
+                                        selectedItemIndex = index
+                                        // navController.navigate(item.title)
+                                    },
+                                    label = {
+                                        Text(text = item.title)
+                                    },
+                                    icon = { 
+                                        BadgedBox(
+                                            badge = {
+                                                if(item.hasNews){
+                                                    Badge()
+                                                }
+                                            }
+                                        ) {
+                                            Icon(
+                                                imageVector = if (index == selectedItemIndex) {
+                                                    item.selectedIcon
+                                                } else item.unselectedIcon,
+                                                contentDescription = item.title,
+                                                modifier = Modifier
+                                                    .size(25.dp)
+                                            )
+                                        }
+                                    })
+                            }
+
+                        }
+                    }
                 ) { innerPadding ->
                     Column(
                         modifier = Modifier
