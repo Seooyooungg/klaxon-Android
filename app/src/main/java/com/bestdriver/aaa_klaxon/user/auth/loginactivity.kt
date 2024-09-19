cc0e37c55b58d173049358fd73d03cd611cfa38f
Jaen-923 <113357550+Jaen-923@users.noreply.github.com>
2024-09-19 13:07:06 +0900

    Feat 로그인 UI 개선

--- app/src/main/java/com/bestdriver/aaa_klaxon/user/auth/loginactivity.kt
+++ app/src/main/java/com/bestdriver/aaa_klaxon/user/auth/loginactivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -76,115 +77,133 @@ fun LoginScreen(
     var password by remember { mutableStateOf("") }
     var loginError by remember { mutableStateOf("") }
 
-    Column(
+    // Box를 사용하여 Column을 중앙에 배치
+    Box(
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp)
-            .background(Color.White),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+            .background(Color.White)
+            .padding(16.dp),
+        contentAlignment = Alignment.Center // Box의 자식 컨텐츠를 중앙에 정렬
     ) {
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                    .fillMaxWidth()
+                .padding(bottom = 100.dp)
         ) {
-            Text(
-                text = "클락션",
-                fontSize = 40.sp,
-                color = MyPurple,
-                fontFamily = FontFamily(Font(R.font.pretendard_extrabold)),
-                textAlign = TextAlign.Center,
-                modifier = Modifier.padding(bottom = 8.dp)
-            )
-            Text(
-                text = "Klaxon",
-                fontSize = 30.sp,
-                color = MyPurple,
-                fontFamily = FontFamily(Font(R.font.pretendard_semibold)),
-                textAlign = TextAlign.Center
-            )
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        OutlinedTextField(
-            value = username,
-            onValueChange = { username = it },
-            label = { Text("아이디", fontSize = 20.sp) },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-        )
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("비밀번호", fontSize = 20.sp) },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp),
-            visualTransformation = PasswordVisualTransformation()
-        )
-
-        if (loginError.isNotEmpty()) {
-            Text(
-                text = loginError,
-                color = Color.Red,
-                fontSize = 16.sp,
-                modifier = Modifier.padding(vertical = 8.dp),
-                fontFamily = FontFamily(Font(R.font.pretendard_medium))
-            )
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(
-            onClick = {
-                if (username.isNotBlank() && password.isNotBlank()) {
-                    // 아이디와 비밀번호가 모두 입력된 경우
-                    onLoginSuccess()
-                } else {
-                    // 아이디 또는 비밀번호가 비어있는 경우
-                    loginError = "아이디와 비밀번호를 입력해 주세요."
-                }
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-                .height(56.dp), // 버튼의 높이 설정
-            colors = ButtonDefaults.buttonColors(
-                containerColor = MyPurple
-            )
-        ) {
-            Text("로그인",
-                fontSize = 20.sp,
-                color = Color.White,
-                fontFamily = FontFamily(Font(R.font.pretendard_medium)),
-            )
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(
-                text = "회원가입",
+            // 제목
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
-                    .clickable { navController.navigate("signup") }
-                    .padding(end = 16.dp),
-                color = MyPurple,
-                fontSize = 20.sp,
-                fontFamily = FontFamily(Font(R.font.pretendard_regular)),
-                textAlign = TextAlign.Center
+                    .fillMaxWidth()
+                    .padding(bottom = 30.dp)
+            ) {
+                Text(
+                    text = "클락션",
+                    fontSize = 45.sp,
+                    color = MyPurple,
+                    fontFamily = FontFamily(Font(R.font.pretendard_extrabold)),
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+                Text(
+                    text = "Klaxon",
+                    fontSize = 35.sp,
+                    color = MyPurple,
+                    fontFamily = FontFamily(Font(R.font.pretendard_semibold)),
+                    textAlign = TextAlign.Center
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // 아이디 입력 필드
+            OutlinedTextField(
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("아이디", fontSize = 20.sp) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp)
             )
-            Text(
-                text = "아이디/비밀번호 찾기",
-                modifier = Modifier.clickable { /* 아이디/비밀번호 찾기 로직 추가 */ },
-                color = MyPurple,
-                fontSize = 23.sp,
-                fontFamily = FontFamily(Font(R.font.pretendard_regular)),
-                textAlign = TextAlign.Center
+            // 비밀번호 입력 필드
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("비밀번호", fontSize = 20.sp) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                visualTransformation = PasswordVisualTransformation()
             )
+
+            // 로그인 오류 메시지
+            if (loginError.isNotEmpty()) {
+                Text(
+                    text = loginError,
+                    color = Color.Red,
+                    fontSize = 16.sp,
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    fontFamily = FontFamily(Font(R.font.pretendard_medium))
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // 로그인 버튼
+            Button(
+                onClick = {
+                    if (username.isNotBlank() && password.isNotBlank()) {
+                        // 아이디와 비밀번호가 모두 입력된 경우
+                        onLoginSuccess()
+                    } else {
+                        // 아이디 또는 비밀번호가 비어있는 경우
+                        loginError = "아이디와 비밀번호를 입력해 주세요."
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 25.dp)
+                    .height(56.dp),
+                shape = RoundedCornerShape(5.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MyPurple
+                )
+            ) {
+                Text("로그인",
+                    fontSize = 21.sp,
+                    color = Color.White,
+                    fontFamily = FontFamily(Font(R.font.pretendard_medium)),
+                )
+            }
+
+            Spacer(modifier = Modifier.height(2.dp))
+
+            // 하단 링크
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+
+            ) {
+                Text(
+                    text = "회원가입",
+                    modifier = Modifier
+                        .clickable { navController.navigate("signup") }
+                        .padding(end = 16.dp),
+                    color = MyPurple,
+                    fontSize = 18.sp,
+                    fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = "아이디/비밀번호 찾기",
+                    modifier = Modifier.clickable { /* 아이디/비밀번호 찾기 로직 추가 */ },
+                    color = MyPurple,
+                    fontSize = 18.sp,
+                    fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+                    textAlign = TextAlign.Center
+                )
+            }
         }
     }
 }
